8db39d13ca1caa969414448ffd6fef9497125295
dzisinho <dzisbogdan26@gmail.com>
2024-12-18 12:01:39 +0200

    completed

--- 2masiv.cpp
+++ 2masiv.cpp
@@ -1,1 +1,35 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols, startValue;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    cout << "Enter the number of columns: ";
+    cin >> cols;
+    cout << "Enter the starting number: ";
+    cin >> startValue;
+
+    int array[100][100]; 
+    array[0][0] = startValue; 
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (i == 0 && j == 0) continue; 
+            if (j == 0) {
+                array[i][j] = array[i - 1][cols - 1] * 2; 
+            }
+            else {
+                array[i][j] = array[i][j - 1] * 2; 
+            }
+        }
+    }
+    cout << "Generated array:" << endl;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << array[i][j] << "\t";
+        }
+        cout << endl;
+    }
+    return 0;
+}
